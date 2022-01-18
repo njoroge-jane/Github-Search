cfeb1ee73a796aa3d0bb74279c59e1d6259f05bc
Jane Njoroge <jane.njoroge@student.moringaschool.com>
2022-01-18 23:09:21 +0300

    add user class for profiles

---
+++ src/app/user.spec.ts
@@ -0,0 +1,7 @@
+import { Users } from './user';
+
+describe('User', () => {
+  it('should create an instance', () => {
+    expect(new Users()).toBeTruthy();
+  });
+});
---
+++ src/app/user.ts
@@ -0,0 +1,7 @@
+export class Users {
+  public login!: string;
+  public html_url!: string;
+  public followers!: number;
+  public following!: number;
+  public created_on!: Date;
+}
