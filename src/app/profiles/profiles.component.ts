7307e516bb377f7d8a19eaf15fc184390d4ac4af
Jane Njoroge <jane.njoroge@student.moringaschool.com>
2022-01-19 02:58:11 +0300

    make style updates

--- src/app/profiles/profiles.component.ts
+++ src/app/profiles/profiles.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProfileService }from '../profiles.service';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-profile',
