c0b491aae8952c35850017ffbc25e71ce86d380e
Jane Njoroge <jane.njoroge@student.moringaschool.com>
2022-01-18 23:10:43 +0300

    add repo class for repos

---
+++ src/app/repo.spec.ts
@@ -0,0 +1,7 @@
+import { Repos } from './repo';
+
+describe('Repositories', () => {
+  it('should create an instance', () => {
+    expect(new Repos).toBeTruthy();
+  });
+});
---
+++ src/app/repo.ts
@@ -0,0 +1,7 @@
+export class Repos {
+  public name!: string;
+  public html_url!: string;
+  public description!: string;
+  public created_at!: Date;
+  public deployments_url!: string;
+}
