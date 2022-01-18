889fcdcc04a46b3121ec8047e9cc2f6b38563898
Jane Njoroge <jane.njoroge@student.moringaschool.com>
2022-01-18 23:11:07 +0300

    add a service

---
+++ src/app/profiles.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { ProfileService } from './profiles.service';
+
+describe('ProfilesService', () => {
+  let service: ProfileService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(ProfileService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
---
+++ src/app/profiles.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient }from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProfileService {
+
+  private login:string;
+  private id = "91135600";
+  private client_secret ="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";
+  constructor(private http:HttpClient) { 
+    this.login = "njoroge_jane";
+  }
+  getProfileInfo(){
+    return this.http.get("https://api.github.com/users/" + this.login + "?client_id=" + this.id + "&client_secret=" + this.client_secret)
+  }
+  getProfileRepos(){
+    return this.http.get("https://api.github.com/users/" + this.login + "/repos?client_id="+ this.id + "&client_secret=" + this.client_secret)
+  }
+  updateProfile(login:string){
+    this.login = login;
+  }
+}
